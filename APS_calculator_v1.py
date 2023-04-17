328b7a6b8f58644a5b4cb6abd8b3904b8cdfdefe
hikmetc <40365975+hikmetc@users.noreply.github.com>
2023-04-17 13:00:23 +0300

    Update APS_calculator_v1.py

--- APS_calculator_v1.py
+++ APS_calculator_v1.py
@@ -192,8 +192,8 @@ if analyze_button:
 
             elif number_CDL == 3:
                 bins = [0, cdl_1-0.000001, cdl_2, cdl_3, np.inf]
-                names = [f'<{cdl_1}', f'{cdl_1}-{cdl_2}', 
-                         f'{cdl_2 + Decimal('1') / (Decimal('10') ** Decimal(str(input_value).count('.') - 1))}-{cdl_3}' ,f'>{cdl_3}']
+                cdl_22 = cdl_2+ Decimal('1') / (Decimal('10') ** Decimal(str(cdl_2).count('.') - 1))
+                names = [f'<{cdl_1}', f'{cdl_1}-{cdl_2}', f'{cdl_22}-{cdl_3}' ,f'>{cdl_3}']
                 value = [1, 2, 3, 4]
 
             elif number_CDL == 4:
