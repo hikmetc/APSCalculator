d2d27d1b6ec9592f7e123558e6ef21c2d8161a5f
hikmetc <40365975+hikmetc@users.noreply.github.com>
2023-04-17 14:44:46 +0300

    Update APS_calculator_v1.py

--- APS_calculator_v1.py
+++ APS_calculator_v1.py
@@ -179,7 +179,12 @@ def count_decimal_places(number):
     else:
         num_decimal_places = 1 
     return num_decimal_places
-                
+
+def check_integer(cdl_st):
+    if count_decimal_places(cdl_st) == 0:
+        return int(cdl_st)
+    else:
+        return cdl_st
           
 # action after clicking the button "simulate & analyze" 
 if analyze_button:
@@ -202,7 +207,8 @@ if analyze_button:
 
             elif number_CDL == 3:
                 bins = [0, cdl_1-0.000001, cdl_2, cdl_3, np.inf]
-                cdl_22 =(cdl_2*(10**(count_decimal_places(cdl_2)))+1) / 10**(count_decimal_places(cdl_2))
+                cdl_st = check_integer(cdl_2)
+                cdl_22 =(cdl_st*(10**(count_decimal_places(cdl_st)))+1) / 10**(count_decimal_places(cdl_st))
 
                 names = [f'<{cdl_1}', f'{cdl_1}-{cdl_2}', f'{cdl_22}-{cdl_3}' ,f'>{cdl_3}']
                 value = [1, 2, 3, 4]
