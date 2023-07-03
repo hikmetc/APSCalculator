5093bcd181c98fded477f74e6b23e73d889ac848
hikmetc <40365975+hikmetc@users.noreply.github.com>
2023-07-03 22:26:15 +0300

    Update APS_calculator_v1.py

--- APS_calculator_v1.py
+++ APS_calculator_v1.py
@@ -397,7 +397,7 @@ if analyze_button:
                 st.markdown('##### **:green[Histogram of the original data]**')
                 # Get unique categories
                 categories_h = analyte_last_df['Analyte_category'].unique()
-                categories_h = sorted([cat for cat in categories_h if cat.startswith("<")]) + sorted([cat for cat in categories_h if not cat.startswith("<")], reverse=True)
+                categories_h = sorted([cat for cat in categories_h if cat.startswith("<")]) + sorted([cat for cat in categories_h if not cat.startswith("<") and not cat.startswith("≥")]) + sorted([cat for cat in categories_h if cat.startswith("≥")])
                 # Create the histogram figure
                 fig_h = go.Figure()             
                 # Iterate over categories and add histogram traces
