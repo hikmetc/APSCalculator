f1209d124912085be2b0e14439b1283d39afd9fa
hikmetc <40365975+hikmetc@users.noreply.github.com>
2023-07-06 14:59:00 +0300

    Update APS_calculator_v1.py

--- APS_calculator_v1.py
+++ APS_calculator_v1.py
@@ -28,10 +28,10 @@ with st.sidebar:
     def process_file(file):
         # data of analyte selection
         try:
-            uploaded_file = pd.read_excel(file) 
+            uploaded_file = pd.read_excel(file)
         except:
-            uploaded_file = pd.read_csv(file, sep=None, engine='python') 
-        analyte_name_box = st.selectbox("**Select Measurand Name**", tuple(uploaded_file.columns))
+            uploaded_file = pd.read_csv(file, sep=None)
+        analyte_name_box = st.selectbox("**Select Analyte Name**", tuple(uploaded_file.columns))
         analyte_data = uploaded_file[analyte_name_box]
         analyte_data = analyte_data.dropna(axis=0).reset_index()
         analyte_data = analyte_data[analyte_name_box]
