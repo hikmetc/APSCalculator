fc05f377ede3eff0cd0b229c1c2701b507d4d1e0
hikmetc <40365975+hikmetc@users.noreply.github.com>
2023-11-12 09:26:10 +0300

    Update APS_calculator_v1.py

--- APS_calculator_v1.py
+++ APS_calculator_v1.py
@@ -120,11 +120,11 @@ with tab1:
                 This web application is designed to help laboratory professinals to determine 
                 their analytical performance specifications for relative standard measurement uncertainty based on their intended clinical setting and population of concern.
                 """)
-    st.info("""*Please cite:* Çubukçu, H.C., Vanstapel, F., Thelen M., van Schrojenstein Lantman, M., 
+    st.info("""**Please cite:** Çubukçu, H.C., Vanstapel, F., Thelen M., van Schrojenstein Lantman, M., 
     Bernabeu-Andreu, F. A., Mesko Brguljan P., Milinkovic, N., Linko, S., Panteghini, M., Boursier, G. 
     (2023) APS Calculator: A Data-Driven Tool for Setting Outcome-Based Analytical Performance Specifications 
     for Measurement Uncertainty Using Specific Clinical Requirements and Population Data. Clinical Chemistry 
-    and Laboratory Medicine (CCLM), (Ahead of a print) <doi: 10.1515/cclm-2023-0740>""")
+    and Laboratory Medicine (CCLM), (Ahead of a print) doi: <10.1515/cclm-2023-0740>""")
     
     st.markdown("""            
                 #### Instructions
