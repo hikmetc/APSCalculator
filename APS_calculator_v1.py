393776dc02402e66cd9b554b0b7db46d4c93708e
hikmetc <40365975+hikmetc@users.noreply.github.com>
2024-02-17 15:18:45 +0300

    Update APS_calculator_v1.py

--- APS_calculator_v1.py
+++ APS_calculator_v1.py
@@ -119,6 +119,9 @@ with tab1:
     st.markdown("""
                 This web application is designed to help laboratory professinals to determine 
                 their analytical performance specifications for relative standard measurement uncertainty based on their intended clinical setting and population of concern.
+                
+                If you would like to use on your own PC as a desktop application download the exe file below:
+                **Desktop application download lint for windows:** [APS_Calculator_v_0.1.0.exe](https://drive.google.com/file/d/1-SjlhLRAFANJns6OXDWVaXjEhUDNkbDd/view?usp=sharing)
                 """)
     st.markdown("""            
                 #### Instructions
