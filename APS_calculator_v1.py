660efad9dc9bac2ec7cc8db89609a1e58d607263
Hikmet Can Çubukçu <40365975+hikmetc@users.noreply.github.com>
2024-10-03 15:02:32 +0300

    Update APS_calculator_v1.py

--- APS_calculator_v1.py
+++ APS_calculator_v1.py
@@ -25,7 +25,7 @@ with st.sidebar:
                         mime='application/octet-stream')
     
     # DATA UPLOAD AND COLUMN SELECTION
-    @st.cache_resource
+    
     def process_file(file):
         # data of analyte selection
         try:
